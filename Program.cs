5183d96803452e66df0a0859b1d413927e0a82fb
CyberCarpenter1 <mattholiveira8@gmail.com>
2026-04-13 19:53:58 -0300

    Treinando os pilares do POO
    Abstraindo um jogo de RPG, utilizando orientação a objetos com C#

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Abstraindo_um_jogo_de_RPG__utilizando_orientação_a_objetos_com_C_;
+using Abstraindo_um_jogo_de_RPG__utilizando_orientação_a_objetos_com_C_.src.src;
+using Abstraindo_um_jogo_de_RPG__utilizando_orientação_a_objetos_com_C_.src.src.Entities;
+
+
+
+class HeroTeam
+{
+    static void Main(string[] args)
+    {
+        Knight Artorias = new Knight("Artorias", 10, "Guerreiro");
+        Wizard Frieren = new Wizard("Frieren", 1, "Mago");
+        Archer Legolas = new Archer("Legolas", 5, "Arqueiro");
+
+        System.Console.WriteLine(Artorias.attack());
+        System.Console.WriteLine(Frieren.attack());
+        System.Console.WriteLine(Legolas.attack());
+    }
+}
